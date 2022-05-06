369b3ece4bc2052679a3dc69f49f9114ee5410b5
Madalina-Alexandra <mm2086@sussex.ac.uk>
2022-05-06 13:52:49 +0100

    Added gallery images and thumbnails

--- src/views/organisms/gallery/gallery.jsx
+++ src/views/organisms/gallery/gallery.jsx
@@ -2,16 +2,27 @@ import React, { useState, useContext } from "react";
 import "./gallery.scss";
 import { MainAppModel } from "../../../models/main.model";
 import Image from "../../atoms/image";
-import galleryImage1 from "../../../images/gallery-img-1.jpg";
-import galleryImage2 from "../../../images/gallery-img-2.jpg";
-import galleryImage3 from "../../../images/gallery-img-3.jpg";
-import galleryImage4 from "../../../images/gallery-img-1.jpg";
-import galleryImage5 from "../../../images/gallery-img-2.jpg";
-import galleryImage6 from "../../../images/gallery-img-3.jpg";
-import galleryImage7 from "../../../images/gallery-img-1.jpg";
-import galleryImage8 from "../../../images/gallery-img-2.jpg";
-import galleryImage9 from "../../../images/gallery-img-3.jpg";
+import galleryImage1 from "../../../images/gallery/full/full-1.jpg";
+import galleryImage2 from "../../../images/gallery/full/full-2.jpg";
+import galleryImage3 from "../../../images/gallery/full/full-3.jpg";
+import galleryImage4 from "../../../images/gallery/full/full-4.jpg";
+import galleryImage5 from "../../../images/gallery/full/full-5.jpg";
+import galleryImage6 from "../../../images/gallery/full/full-6.jpg";
+import galleryImage7 from "../../../images/gallery/full/full-7.jpg";
+import galleryImage8 from "../../../images/gallery/full/full-8.jpg";
+import galleryImage9 from "../../../images/gallery/full/full-9.jpg";
+import galleryThumbImage1 from "../../../images/gallery/thumb/thumb-1.jpg";
+import galleryThumbImage2 from "../../../images/gallery/thumb/thumb-2.jpg";
+import galleryThumbImage3 from "../../../images/gallery/thumb/thumb-3.jpg";
+import galleryThumbImage4 from "../../../images/gallery/thumb/thumb-4.jpg";
+import galleryThumbImage5 from "../../../images/gallery/thumb/thumb-5.jpg";
+import galleryThumbImage6 from "../../../images/gallery/thumb/thumb-6.jpg";
+import galleryThumbImage7 from "../../../images/gallery/thumb/thumb-7.jpg";
+import galleryThumbImage8 from "../../../images/gallery/thumb/thumb-8.jpg";
+import galleryThumbImage9 from "../../../images/gallery/thumb/thumb-9.jpg";
+
 import ChevronButton from "../../atoms/buttons/chevron-button";
+
 import SecondaryButton from "../../atoms/buttons/secondary-button";
 
 const Gallery = () => {
@@ -19,15 +30,56 @@ const Gallery = () => {
   const { model, dispatch } = useContext(MainAppModel);
 
   const images = [
-    { figcaption: "Glaceau render", src: galleryImage1 },
-    { figcaption: "Costa render", src: galleryImage2 },
-    { figcaption: "Coke render", src: galleryImage3 },
-    { figcaption: "Glaceau render", src: galleryImage4 },
-    { figcaption: "Costa render", src: galleryImage5 },
-    { figcaption: "Coke render", src: galleryImage6 },
-    { figcaption: "Glaceau render", src: galleryImage7 },
-    { figcaption: "Costa render", src: galleryImage8 },
-    { figcaption: "Coke render", src: galleryImage9 },
+    {
+      figcaption:
+        "Glaceau Water Bottles rendered & textured with Redshift for C4D",
+      full: galleryImage2,
+      thumb: galleryThumbImage2,
+    },
+    {
+      figcaption:
+        "Glaceau Water Bottles rendered & textured with Redshift for C4D",
+      full: galleryImage3,
+      thumb: galleryThumbImage3,
+    },
+    {
+      figcaption: "Coke can top rendered & textured with Redshift for C4D",
+      full: galleryImage4,
+      thumb: galleryThumbImage4,
+    },
+    {
+      figcaption: "Coke cans rendered & textured with Redshift for C4D",
+      full: galleryImage5,
+      thumb: galleryThumbImage5,
+    },
+    {
+      figcaption: "Costa Coffee cans rendered & textured with Redshift for C4D",
+      full: galleryImage7,
+      thumb: galleryThumbImage7,
+    },
+    {
+      figcaption:
+        "Coke can textures recreated using Adobe Illustrator and Photoshop",
+      full: galleryImage6,
+      thumb: galleryThumbImage6,
+    },
+    {
+      figcaption:
+        "Costa can textures recreated using Adobe Illustrator and Photoshop",
+      full: galleryImage8,
+      thumb: galleryThumbImage8,
+    },
+    {
+      figcaption:
+        "Water bottle textures recreated using Adobe Illustrator and Photoshop",
+      full: galleryImage9,
+      thumb: galleryThumbImage9,
+    },
+    {
+      figcaption: "All models rendered & textured with Redshift for C4D",
+      full: galleryImage1,
+      thumb: galleryThumbImage1,
+    },
   ];
 
   // For mobile
@@ -58,7 +110,7 @@ const Gallery = () => {
   const handleShowModal = (image) =>
     dispatch({
       type: "SET_MODAL",
-      payload: { src: image.src, alt: image.figcaption },
+      payload: { src: image.full, alt: image.figcaption },
     });
 
   return (
@@ -66,7 +118,7 @@ const Gallery = () => {
       {/* Mobile */}
       <div className="gallery__container gallery__container--mobile">
         <Image
-          src={currentImage.src}
+          src={currentImage.thumb}
           alt={currentImage.figcaption}
           figcaption={currentImage.figcaption}
         />
@@ -87,7 +139,7 @@ const Gallery = () => {
         {[...images.slice(0, amountOfImagesToShow)].map((image) => (
           <Image
             key={crypto.randomUUID()}
-            src={image.src}
+            src={image.thumb}
             alt={image.figcaption}
             figcaption={image.figcaption}
             onClick={() => handleShowModal(image)}
