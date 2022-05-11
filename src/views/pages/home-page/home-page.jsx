2af7525ecf1be3915b986a82b0d22b504c37bdde
Madalina-Alexandra <mm2086@sussex.ac.uk>
2022-05-11 22:06:12 +0100

    Fixed logo and bg image bug. Added build folder to repo just in case

--- src/views/pages/home-page/home-page.jsx
+++ src/views/pages/home-page/home-page.jsx
@@ -4,6 +4,11 @@ import Hero from "../../organisms/hero";
 
 const HomePage = () => (
   <div className="home-page">
+    <img
+      className="home-page__bg"
+      alt=""
+      src={process.env.PUBLIC_URL + "images/backgrounds/hero-desktop-bg.svg"}
+    />
     <main className="home-page__container">
       <Hero />
     </main>
